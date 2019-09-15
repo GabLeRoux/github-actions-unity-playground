5505702bdfedccaf51f297ba81ac4cb7b61f3d96
Artalus <artalus-mail@yandex.ru>
2019-09-15 12:28:39 +0300

    check that license present

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -5,6 +5,8 @@ set -x
 mkdir -p /root/.cache/unity3d
 mkdir -p /root/.local/share/unity3d/Unity/
 set +x
+echo 'Checking that UNITY_LICENSE_CONTENT is present'
+[[ -n "$UNITY_LICENSE_CONTENT" ]] # assert that license content is present
 echo 'Writing $UNITY_LICENSE_CONTENT to license file /root/.local/share/unity3d/Unity/Unity_lic.ulf'
 echo "$UNITY_LICENSE_CONTENT" | tr -d '\r' > /root/.local/share/unity3d/Unity/Unity_lic.ulf
 
